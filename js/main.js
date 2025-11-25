dcb713d8e448cd6e0051e1d3ed090d35eca89422
hinako-mochizuki <t.ab.love1024@gmail.com>
2025-11-25 19:19:11 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$(function () {
+  $('a[href^="#"]').click(function () {
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, 500, "swing");
+    return false;
+  });
+
+  $(".hamburger").on("click", function () {
+    if ($("header").hasClass("open")) {
+      $("header").removeClass("open");
+    } else {
+      $("header").addClass("open");
+    }
+  });
+
+  $('a[href^="#"]').on("click", function () {
+    $("header").removeClass("open");
+  });
+
+  $(window).scroll(function () {
+    // fadeinクラスに対して順に処理を行う
+    $(".fadein").each(function () {
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+      // 画面の高さ
+      let windowHeight = $(window).height();
+      // fadeinクラスの要素が画面下にきてから200px通過した
+      // したタイミングで要素を表示
+      if (scroll > target - windowHeight + 100) {
+        $(this).css("opacity", "1");
+        $(this).css("transform", "translateY(0)");
+      }
+    });
+  });
+    
+    $(window).scroll(function () {
+      // fadeinクラスに対して順に処理を行う
+      $(".left-item").each(function () {
+        // スクロールした距離
+        let scroll = $(window).scrollTop();
+        // fadeinクラスの要素までの距離
+        let target = $(this).offset().top;
+        // 画面の高さ
+        let windowHeight = $(window).height();
+        // fadeinクラスの要素が画面下にきてから200px通過した
+        // したタイミングで要素を表示
+        if (scroll > target - windowHeight + 5) {
+          $(this).addClass("slide-left");
+        }
+      });
+    });
+
+  $(window).scroll(function () {
+    $(".right-item").each(function () {
+      var scroll = $(window).scrollTop();
+
+      var target = $(this).offset().top;
+
+      var windowHeight = $(window).height();
+
+      if (scroll > target - windowHeight + $(this).outerHeight()) {
+        // outerHeight()はpaddingを含めた高さを取得する
+        $(this).addClass("slide-right");
+      }
+    });
+  });
+});
